3b60440671ed1fa40d38c2ed1ba5cc4289a28716
Akshay Tekam <akshaytekam@atlassian.com>
2020-11-13 20:54:56 +0530

    First Commit

---
+++ CompanyEmpWage.java
@@ -0,0 +1,35 @@
+ 
+package daytwentytwo;
+
+ 
+public class CompanyEmpWage
+{
+
+public final String company;
+public final int empRatePerHour;
+public final int numOfWorkingDays;
+public final int maxHoursPerMonth;
+public int totalEmpWage;
+
+public CompanyEmpWage(String company, int empRatePerHour, int numOfWorking Days, int maxHoursPerMonth)
+{
+
+this.company = company;
+this.empRatePerHour = empRatePerHour;
+this.numOfWorkingDays = numOfWorkingDays;
+this.maxHoursPerMonth = maxHoursPerMonth;
+
+}
+
+public void setTotalEmpWage(int totalEmpWage)
+{
+	this.totalEmpWage = totalEmpWage;
+}
+
+@Override
+public String toString()
+	{
+	return "Total Emp Wage for Company: " +company+" is: "+totalEmpWage;
+	}
+}
+
